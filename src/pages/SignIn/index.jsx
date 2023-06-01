552aea67b41209b87c5d15f3a9a5d34d253402c6
olmeiro <olme.orozco@gmail.com>
2023-06-01 15:58:46 -0500

    feat: login design
    First approach login page, in fact could be great to define Navbar to this component. I'm stucked trying to put an image inside

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,110 +1,72 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import Navbar from '../../components/Navbar';
+import Footer from '../../components/Footer';
 
 const SignIn = () => {
   return (
     <div className="flex flex-col">
-      <section>
-        <div className="relative flex justify-center max-h-full overflow-hidden lg:px-0 md:px-12">
-          <div className="relative z-10 flex flex-col flex-1 px-4 py-10 bg-white shadow-2xl lg:py-24 md:flex-none md:px-28 sm:justify-center">
-            <div className="w-full max-w-md mx-auto md:max-w-sm md:px-0 md:w-96 sm:px-4">
-              <div className="flex flex-col">
-                <div>
-                  <h2 className="text-4xl text-black">Let's get started!</h2>
-                  <p className="mt-2 text-sm text-gray-500">
-                    Complete the details below so I can process your request and
-                    then schedule a time to discuss your goals.
-                  </p>
-                </div>
+      <Navbar />
+      <div className="flex flex-col sm:m-16 md:m-0">
+        <div className="w-full max-w-md mx-auto md:mt-28">
+          <div className="flex flex-col">
+            <div>
+              <h2 className="text-4xl text-black text-center">
+                Let's get started!
+              </h2>
+            </div>
+          </div>
+          <form>
+            <div className="mt-4 space-y-6">
+              <div className="col-span-full">
+                <label
+                  className="block mb-3 text-sm font-medium text-gray-600"
+                  name="email"
+                >
+                  Email
+                </label>
+                <input
+                  className="block w-full px-6 py-3 text-black bg-white border border-gray-200 appearance-none rounded-xl placeholder:text-gray-400 focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
+                  placeholder="email@example.com"
+                  autocomplete="off"
+                  type="email"
+                />
               </div>
-              <form>
-                <input autocomplete="false" name="hidden" />
-                <input name="_redirect" type="hidden" value="#" />
-                <div className="mt-4 space-y-6">
-                  <div>
-                    <label
-                      className="block mb-3 text-sm font-medium text-gray-600"
-                      name="name"
-                    >
-                      First name
-                    </label>
-                    <input
-                      className="block w-full px-6 py-3 text-black bg-white border border-gray-200 appearance-none rounded-xl placeholder:text-gray-400 focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
-                      placeholder="Your name"
-                    />
-                  </div>
-                  <div className="col-span-full">
-                    <label
-                      className="block mb-3 text-sm font-medium text-gray-600"
-                      name="company"
-                    >
-                      What is the name of your company / organisation?
-                    </label>
-                    <input
-                      className="block w-full px-6 py-3 text-black bg-white border border-gray-200 appearance-none rounded-xl placeholder:text-gray-400 focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
-                      placeholder="Company name"
-                    />
-                  </div>
-                  <div className="col-span-full">
-                    <label
-                      className="block mb-3 text-sm font-medium text-gray-600"
-                      name="email"
-                    >
-                      How shall we contact you?
-                    </label>
-                    <input
-                      className="block w-full px-6 py-3 text-black bg-white border border-gray-200 appearance-none rounded-xl placeholder:text-gray-400 focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
-                      placeholder="email@example.com"
-                      autocomplete="off"
-                      type="email"
-                    />
-                  </div>
-                  <div>
-                    <div>
-                      <label
-                        className="block mb-3 text-sm font-medium text-gray-600"
-                        name="message"
-                      >
-                        Project details
-                      </label>
-                      <div className="mt-1">
-                        <textarea
-                          className="block w-full px-6 py-3 text-black bg-white border border-gray-200 appearance-none rounded-xl placeholder:text-gray-400 focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
-                          placeholder="What are you working on?"
-                          rows="4"
-                        ></textarea>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-span-full">
-                    <button
-                      className="items-center justify-center w-full px-6 py-2.5 text-center text-white duration-200 bg-black border-2 border-black rounded-full nline-flex hover:bg-transparent hover:border-black hover:text-black focus:outline-none focus-visible:outline-black text-sm focus-visible:ring-black"
-                      type="submit"
-                    >
-                      Submit your request
-                    </button>
-                  </div>
-                </div>
-              </form>
+              <div className="col-span-full">
+                <label
+                  className="block mb-3 text-sm font-medium text-gray-600"
+                  name="company"
+                >
+                  Password
+                </label>
+                <input
+                  className="block w-full px-6 py-3 text-black bg-white border border-gray-200 appearance-none rounded-xl placeholder:text-gray-400 focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
+                  placeholder="Password"
+                  type="password"
+                />
+              </div>
+              <div className="col-span-full">
+                <button
+                  className="items-center justify-center w-full px-6 py-2.5 text-center text-white duration-200 bg-black border-2 border-black rounded-full nline-flex hover:bg-transparent hover:border-black hover:text-black focus:outline-none focus-visible:outline-black text-sm focus-visible:ring-black"
+                  type="submit"
+                >
+                  Login
+                </button>
+              </div>
             </div>
-          </div>
-          <div className="hidden bg-white lg:block lg:flex-1 lg:relative sm:contents">
-            <div
-              className="absolute inset-0 object-cover w-full h-full bg-white"
-              alt=""
-              height="1866"
-              width="1664"
-            >
-              <img
-                className="object-center w-full h-auto bg-gray-200"
-                src="https://d33wubrfki0l68.cloudfront.net/64c901dbc4b16388ef27646a320ad9c1441594df/236fd/images/placeholders/rectangle2.svg"
-                alt=""
-                width="1310"
-                height="873"
-              />
-            </div>
-          </div>
+          </form>
         </div>
-      </section>
+        <div className="col-span-full">
+          <NavLink to="/sign-up">
+            <label
+              className="block mt-8 text-center text-sm font-medium text-blue-500"
+              name="email"
+            >
+              Don't have account yet?
+            </label>
+          </NavLink>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 };
