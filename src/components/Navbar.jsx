459ce60c8e569bb8298e1f3797c4bbdfc3f90d21
olmeiro <olme.orozco@gmail.com>
2023-06-03 14:00:55 -0500

    feat: design sign up
    also I changed the Book icon at the Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,25 @@ const Navbar = () => {
             to="/"
           >
             <span className="lg:text-lg uppercase focus:ring-0">
-              <ion-icon name="book"></ion-icon>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="icon icon-tabler icon-tabler-book"
+                width="44"
+                height="44"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="#2c3e50"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M3 19a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />
+                <path d="M3 6a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />
+                <path d="M3 6l0 13" />
+                <path d="M12 6l0 13" />
+                <path d="M21 6l0 13" />
+              </svg>
             </span>
           </Link>
           {/* Button mobile */}
