5bc23ce02149618869ea93e179f0e40d82cb9c35
Olmeiro <olme.orozco@gmail.com>
2023-07-04 10:46:13 -0500

    fix bugs

--- src/pages/AI/index.jsx
+++ src/pages/AI/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import UserBooksPreferencesForm from '../../components/forms/userBooksPreferencesForm';
 import { GlobalContext } from '../../store/globalContext';
 
